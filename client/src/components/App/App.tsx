9151727b8caf165180b1ef4ccd915f314a1f9028
yarden zehavi <yarden.goldy@gmail.com>
2022-08-09 17:46:49 +0300

    client : add ui matrix

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,9 +1,17 @@
+import { IGame} from 'app-shared-types';
 import React from 'react';
 import './App.scss';
 import {createApiClient} from './AppApi';
 
 export type AppState = {
 	search: string;
+	game: IGame
+}
+
+export enum state
+{
+    unpopulated = 0,
+    populated = 1
 }
 
 const api = createApiClient();
@@ -11,12 +19,18 @@ const api = createApiClient();
 export class App extends React.PureComponent<{}, AppState> {
 
 	state: AppState = {
-		search: ''
+		search: '',
+		game: {
+			board: []
+		}
 	}
 
 	searchDebounce: any = null;
 
 	async componentDidMount() {
+		this.setState({
+			game: await api.getGame()
+		});
 	}
 
 	onSearch = async (val: string, newPage?: number) => {
@@ -29,13 +43,69 @@ export class App extends React.PureComponent<{}, AppState> {
 			});
 		}, 300);
 	}
+	onClickCell = (row:number, col:number) =>{
+		this.setState(prevState => {
+			let game = Object.assign({}, prevState.game);
+			const board = game.board;
+			if(board[row][col] === state.unpopulated)
+				board[row][col] = state.populated;
+			else
+			{
+				board[row][col] = state.populated;
+			}
+			return {game};
+		});
+
+		console.log(row, col);
+	}
+
+	 onclickNext = async () => {
+		const game = await api.getNextStep(this.state.game);
+		this.setState({game});
+	}
+
+	onclickReset = () => {
+
+	}
+
+	onclickStart = () => {
+
+	}
+
+	onclickStop = () => {
+
+	}
+
+	renderGame = (game: IGame) => {
+		const board = game.board;
+		let count = 0;
+		return (<div className='board'>
+			{
+				board.map((rowValue, rowIndex) => (
+					<div key={rowIndex} className='rows'>
+						{rowValue.map((colValue, colIndex) => (
+							<button className={'cell-button ' + (board[rowIndex][colIndex] === state.unpopulated ? "unpopulated" : "populated")} key={count++} onClick={this.onClickCell.bind(this, rowIndex, colIndex)}></button>
+						))}
+					</div>
+				))
+			}
+		</div>);
+	}
 
 	render() {	
+		const {game} = this.state;
 		return (<main>
 			<h1>Game Of Life</h1>
 			<header>
 				<input type="search" placeholder="Search..." onChange={(e) => this.onSearch(e.target.value)}/>
 			</header>
+			<div>
+				<button className='next' onClick={this.onclickNext}>Next</button>
+				<button className='reset' onClick={this.onclickReset}>Reset</button>
+				<button className='start' onClick={this.onclickStart}>Start</button>
+				<button className='stop' onClick={this.onclickStop}>Stop</button>
+			</div>
+			{game ? this.renderGame(game) : <h2>Loading..</h2>}
 		</main>)
 	}
 }
--- client/src/components/App/AppApi.ts
+++ client/src/components/App/AppApi.ts
@@ -4,12 +4,16 @@ import axios from 'axios';
 
 export type ApiClient = {
     getGame: () => Promise<IGame>;
+    getNextStep: (game: IGame) => Promise<IGame>;
 }
 
 export const createApiClient = (): ApiClient => {
     return {
-        getGame: () => {
+        getGame: async () => {
             return axios.get(GameAPIRootPath).then((res) => res.data);
-        }
+        },
+        getNextStep: async (game: IGame) => {
+            return axios.post(GameAPIRootPath + "getNextStep", game).then((res) => res.data);
+        }        
     }
 }
