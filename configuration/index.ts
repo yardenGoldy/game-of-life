e137d24fae69d9ab5f70d44705dcacb235c8464b
yarden zehavi <yarden.goldy@gmail.com>
2022-08-09 15:47:41 +0300

    change variables names

--- configuration/index.ts
+++ configuration/index.ts
@@ -1,6 +1,6 @@
 export const serverAPIPort = 3500;
 export const host = 'http://localhost'
-export const TicketAPIDomain = 'game';
-export const TicketAPIRootPath = `${host}:${serverAPIPort}/${TicketAPIDomain}/`
+export const GameAPIDomain = 'game';
+export const GameAPIRootPath = `${host}:${serverAPIPort}/${GameAPIDomain}/`
 export const staticsPort = 3000;
 export const staticsUrl = `${host}:${staticsPort}/`;
